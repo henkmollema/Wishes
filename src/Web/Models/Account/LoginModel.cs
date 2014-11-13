550b2de991dd9e00cea825871906e2a70e8766fd
Henk Mollema <henkmollema@gmail.com>
2014-11-13 19:54:18 +0100

    Added custom display names and error messages for the login view model. Fixed text and classes for register/login buttons. Issue #3, #4.

--- src/Web/Models/Account/LoginModel.cs
+++ src/Web/Models/Account/LoginModel.cs
@@ -4,11 +4,13 @@ namespace Wishes.Web.Models.Account
 {
     public class LoginModel
     {
-        [Required]
+        [Required(ErrorMessage = "Vul je gebruikersnaam in")]
+        [Display(Name = "Gebruikersnaam")]
         public string Username { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Vul je wachtwoord in")]
         [DataType(DataType.Password)]
+        [Display(Name = "Wachtwoord")]
         public string Password { get; set; }
 
         public bool RememberMe { get; set; }
