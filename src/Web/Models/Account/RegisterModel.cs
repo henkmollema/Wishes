3467300d4b941c0c08aa768ea479e84c9f8b0863
Henk Mollema <henkmollema@gmail.com>
2014-11-13 19:45:45 +0100

    Added field names and validation messages - issue #3.

--- src/Web/Models/Account/RegisterModel.cs
+++ src/Web/Models/Account/RegisterModel.cs
@@ -4,28 +4,34 @@ namespace Wishes.Web.Models.Account
 {
     public class RegisterModel
     {
-        [Required]
+        [Required(ErrorMessage = "Gebruikersnaam is verplicht")]
         [MaxLength(50)]
+        [Display(Name = "Gebruikersnaam")]
         public string Username { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Wachtwoord is verplicht")]
         [DataType(DataType.Password)]
+        [Display(Name = "Wachtwoord")]
         public string Password {get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Naam is verplicht")]
         [MaxLength(50)]
+        [Display(Name = "Naam")]
         public string Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Adres is verplicht")]
         [MaxLength(50)]
+        [Display(Name = "Adres")]
         public string Address { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Postcode is verplicht")]
         [MaxLength(6)]
+        [Display(Name = "Postcode")]
         public string Zipcode { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Woonplaats is verplicht")]
         [MaxLength(50)]
+        [Display(Name = "Woonplaats")]
         public string City { get; set; }
     }
 }
